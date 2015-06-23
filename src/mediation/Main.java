015b389ac692184e6506098aa3c42df0754a8001
stebjan <stebjan@kiv.zcu.cz>
2015-06-23 12:39:14 +0200

    tree path created, prepared for comparison

--- src/mediation/Main.java
+++ src/mediation/Main.java
@@ -16,27 +16,27 @@ public class Main {
 
     public static void main(String[] args) throws Exception{
 
-        Tree tree =
-        new WsdlParser().parseXmlFile(new File("C:\\java\\eegdataprocessor\\trunk\\method_output_def\\detection_of_epochs.wsdl"), ParameterType.REQUEST);
-
-        System.out.println(tree.getRoot().getName());
-        System.out.println(tree.getRoot().getChildren().size());
-        System.out.println(tree.getRoot().getChildren().get(0).getName());
-        System.out.println(tree.getRoot().getChildren().get(0).getChildren().get(0).getName());
-        System.out.println(tree.getRoot().getChildren().get(0).getChildren().get(0).getChildren().get(0).getName());
-
-        System.out.println(tree.getRoot().getChildren().get(1).getName());
-        System.out.println(tree.getRoot().getChildren().get(1).getChildren().get(0).getName());
-        System.out.println(tree.getRoot().getChildren().get(1).getChildren().get(0).getChildren().get(0).getName());
-        System.out.println(tree.getRoot().getChildren().get(1).getChildren().get(0).getChildren().get(1).getName());
-        System.out.println(tree.getRoot().getChildren().get(1).getChildren().get(0).getChildren().get(2).getName());
-        System.out.println(tree.getRoot().getChildren().get(1).getChildren().get(0).getChildren().get(0).getChildren().get(0).getName());
-        System.out.println(tree.getRoot().getChildren().get(1).getChildren().get(0).getChildren().get(1).getChildren().get(0).getName());
-        System.out.println(tree.getRoot().getChildren().get(1).getChildren().get(0).getChildren().get(2).getChildren().get(0).getName());
+//        Tree tree =
+//        new WsdlParser().parseXmlFile(new File("C:\\java\\eegdataprocessor\\trunk\\method_output_def\\detection_of_epochs.wsdl"), ParameterType.REQUEST);
+//
+//        System.out.println(tree.getRoot().getName());
+//        System.out.println(tree.getRoot().getChildren().size());
+//        System.out.println(tree.getRoot().getChildren().get(0).getName());
+//        System.out.println(tree.getRoot().getChildren().get(0).getChildren().get(0).getName());
+//        System.out.println(tree.getRoot().getChildren().get(0).getChildren().get(0).getChildren().get(0).getName());
+//
+//        System.out.println(tree.getRoot().getChildren().get(1).getName());
+//        System.out.println(tree.getRoot().getChildren().get(1).getChildren().get(0).getName());
+//        System.out.println(tree.getRoot().getChildren().get(1).getChildren().get(0).getChildren().get(0).getName());
+//        System.out.println(tree.getRoot().getChildren().get(1).getChildren().get(0).getChildren().get(1).getName());
+//        System.out.println(tree.getRoot().getChildren().get(1).getChildren().get(0).getChildren().get(2).getName());
+//        System.out.println(tree.getRoot().getChildren().get(1).getChildren().get(0).getChildren().get(0).getChildren().get(0).getName());
+//        System.out.println(tree.getRoot().getChildren().get(1).getChildren().get(0).getChildren().get(1).getChildren().get(0).getName());
+//        System.out.println(tree.getRoot().getChildren().get(1).getChildren().get(0).getChildren().get(2).getChildren().get(0).getName());
 
         DataMediator mediator = new EEGDataMediator();
         System.out.println(mediator.compatibleParameters("C:\\java\\eegdataprocessor\\trunk\\method_output_def\\detection_of_epochs.wsdl",
-                "C:\\java\\eegdataprocessor\\trunk\\method_output_def\\averaging.wsdl"));
+                "C:\\java\\eegdataprocessor\\trunk\\method_output_def\\detection_of_epochs.wsdl"));
 
     }
 }
--- src/mediation/logic/EEGDataMediator.java
+++ src/mediation/logic/EEGDataMediator.java
@@ -1,11 +1,14 @@
 package mediation.logic;
 
+import mediation.tree.Node;
 import mediation.tree.Tree;
 import org.xml.sax.SAXException;
 
 import javax.xml.parsers.ParserConfigurationException;
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Honza on 20.6.15.
@@ -15,16 +18,46 @@ public class EEGDataMediator implements DataMediator {
     @Override
     public boolean compatibleParameters(File fileOfPreviousMethod, File fileOfNextMethod) throws IOException, SAXException, ParserConfigurationException {
         Tree outputTree = new WsdlParser().parseXmlFile(fileOfPreviousMethod, ParameterType.RESPONSE);
+        List<List<String>> outputTreePaths = new ArrayList<List<String>>();
+        List<List<String>> inputTreePaths = new ArrayList<List<String>>();
         System.out.println("leaves: " + outputTree.getLeaves().size());
+        for (Node leaf: outputTree.getLeaves()) {
+            List<String> outputPath = getNodePathFromLeaf(leaf, ParameterType.RESPONSE);
+            outputTreePaths.add(outputPath);
+            for (String node : outputPath) {
+                System.out.println("node in path: " + node);
+            }
+        }
         Tree inputTree = new WsdlParser().parseXmlFile(fileOfNextMethod, ParameterType.REQUEST);
         System.out.println("leaves: " + inputTree.getLeaves().size());
+        for (Node leaf: inputTree.getLeaves()) {
+            List<String> inputPath = getNodePathFromLeaf(leaf, ParameterType.REQUEST);
+            inputTreePaths.add(inputPath);
+            for (String node : inputPath) {
+                System.out.println("node in path: " + node);
+            }
+        }
 
-        return false;
+        return comparePaths(outputTreePaths, inputTreePaths);
     }
 
     @Override
     public boolean compatibleParameters(String fileOfPreviousMethod, String fileOfNextMethod) throws ParserConfigurationException, SAXException, IOException {
         return compatibleParameters(new File(fileOfPreviousMethod),
                 new File(fileOfNextMethod));
+    }
+
+    private List<String> getNodePathFromLeaf(Node leaf, ParameterType type) {
+        Node current = leaf;
+        List<String> path = new ArrayList<String>();
+        do {
+            path.add(current.getName());
+            current = current.getParent();
+        } while (!current.getName().equals(type.getType()));
+        return path;
+    }
+
+    private boolean comparePaths(List<List<String>> outputPaths, List<List<String>> inputPaths) {
+        return false;
     }
 }
